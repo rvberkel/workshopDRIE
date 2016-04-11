08ff8ec6cba42dc22c9ce71c9dbfbf60e3346b67
NetMore <otten.maarten@gmail.com>
2016-04-11 22:52:00 +0200

    met uml diagram

--- webapp/src/main/java/controller/GlobalControllerExceptionHandler.java
+++ webapp/src/main/java/controller/GlobalControllerExceptionHandler.java
@@ -12,23 +12,18 @@ import org.springframework.web.servlet.ModelAndView;
 
 import daos.AccountDao;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class GlobalControllerExceptionHandler {
 	public static final String DEFAULT_ERROR_VIEW = "error";
 	private static final Logger LOG = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
 
     @ExceptionHandler(value = Exception.class)
     public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
-        // If the exception is annotated with @ResponseStatus rethrow it and let
-        // the framework handle it - like the OrderNotFoundException example
-        // at the start of this post.
-        // AnnotationUtils is a Spring Framework utility class.
         if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
         	LOG.error(e.getMessage());
         	throw e;
         }
         LOG.error(e.getMessage());
-        // Otherwise setup and send the user to a default error-view.
         ModelAndView mav = new ModelAndView();
         mav.addObject("exception", e);
         mav.addObject("url", req.getRequestURL());
